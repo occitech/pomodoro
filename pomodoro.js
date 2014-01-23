201dfe38e249987cfb6e5c4ecb41c9184d5ef2ab
adrien-louis-r <adrien@occitech.fr>
2014-01-23 14:19:16 +0100

    Finished core features: rest and configuration

--- pomodoro.js
+++ pomodoro.js
@@ -1,17 +1,27 @@
 var util = require('util');
 var EventEmitter = require('events').EventEmitter;
 
-var TICK_INTERVAL_MS = 1 * 1000;
-var POMODORO_DURATION_MS = 25 * 1000;
+module.exports = Pomodoro;
 
-var Pomodoro = function() {
+util.inherits(Pomodoro, EventEmitter);
+function Pomodoro(pomodoro_duration_in_min, rest_duration_in_min, tick_interval_in_sec) {
+	this.config = {
+		pomodoro_duration_ms: (pomodoro_duration_in_min || 25) * 60 * 1000,
+		rest_duration_ms: (rest_duration_in_min || 5) * 60 * 1000,
+		tick_interval_ms: (tick_interval_in_sec || 60) * 1000
+	};
+
 	this._started = false;
+	this._resting = false;
 	this._tickInterval = null;
 }
-util.inherits(Pomodoro, EventEmitter);
 
 Pomodoro.prototype.running = function() {
 	return this._started;
+};
+
+Pomodoro.prototype.resting = function() {
+	return this._resting;
 };
 
 Pomodoro.prototype.start = function() {
@@ -22,9 +32,9 @@ Pomodoro.prototype.start = function() {
 
 	this._tickInterval = setInterval(
 		this.tick.bind(this),
-		TICK_INTERVAL_MS
+		this.config.tick_interval_ms
 	);
-	setTimeout(this.stop.bind(this), POMODORO_DURATION_MS);
+	setTimeout(stop.bind(this), this.config.pomodoro_duration_ms);
 
 	this.emit('started', this);
 };
@@ -33,20 +43,25 @@ Pomodoro.prototype.tick = function() {
 	this.emit('ticked', this);
 };
 
-Pomodoro.prototype.stop = function() {
-	if (this.running()) {
-		this._started = false;
-		this.emit('stopped', this);
-	}
-}
-
 Pomodoro.prototype.interrupt = function() {
-    if (!this.running()) {
-        throw new Error('Cannot stop pomodoro already stopped');
-    }
+	if (!this.running()) {
+		throw new Error('Cannot stop pomodoro already stopped');
+	}
 	this._started = false;
 	clearInterval(this._tickInterval);
 	this.emit('interrupted', this);
 }
 
-module.exports = Pomodoro;
+function stop() {
+	if (this.running()) {
+		this._started = false;
+		this._resting = true;
+		this.emit('stopped', this);
+		setTimeout(finishRest.bind(this), this.config.rest_duration_ms);
+	}
+}
+
+function finishRest() {
+	this._resting = false;
+	this.emit('restEnded', this);
+}
--- tests/pomodoro.js
+++ tests/pomodoro.js
@@ -8,6 +8,8 @@ chai.use(sinonChai);
 
 describe('Pomodoro', function() {
 	var subject;
+	var MINUTE_IN_MS = 60 * 1000;
+
 	beforeEach(function () {
 		subject = new Pomodoro;
 	});
@@ -18,6 +20,9 @@ describe('Pomodoro', function() {
 		});
 		it('should not be running', function() {
 			expect(subject.running()).to.be.false;
+		});
+		it('should not be resting', function() {
+			expect(subject.resting()).to.be.false;
 		});
 	});
 
@@ -68,27 +73,53 @@ describe('Pomodoro', function() {
 		});
 
 		it('should be stopped after 25 minutes', function(){
-			clock.tick(25*1000 + 1);
+			clock.tick(25 * MINUTE_IN_MS + 1);
 			expect(subject.running()).to.be.false;
 		});
 		it('should emit stopped event after 25 minutes', function(){
-			 clock.tick(25*1000);
-			expect(stopListener.callCount).to.have.been.call;
+			clock.tick(25 * MINUTE_IN_MS);
+			expect(stopListener).to.have.been.called;
 		});
 
 		it('should pass the pomodoro instance when emitting the stopped event', function(){
-			 clock.tick(25*1000);
+			clock.tick(25 * MINUTE_IN_MS);
 			expect(stopListener).to.have.been.calledWith(subject);
 		});
 
 		it('should emit ticked event every minute', function(){
-			clock.tick(5*1000);
+			clock.tick(5 * MINUTE_IN_MS);
 			expect(tickListener.callCount).to.be.equal(5);
 		});
 		it('should pass the pomodoro instance when emitting the ticked event', function() {
-			clock.tick(5*1000);
+			clock.tick(5 * MINUTE_IN_MS);
 			expect(tickListener).to.have.been.calledWith(subject);
 		});
+
+		describe('rest', function() {
+			var restListener;
+
+			beforeEach(function(){
+				restListener = sinon.spy();
+				subject.on('restEnded', restListener);
+				clock.tick(25 * MINUTE_IN_MS);
+			});
+			it('should emit event to say rest is over after 25 + 5 minutes', function(){
+				clock.tick(5 * MINUTE_IN_MS);
+				expect(restListener).to.have.been.called;
+			});
+			it('should pass the pomodoro instance when emitting the restEnded event', function() {
+				clock.tick(5 * MINUTE_IN_MS);
+				expect(restListener).to.have.been.calledWith(subject);
+			});
+			it('should be resting', function() {
+				expect(subject.resting()).to.be.true;
+			});
+			it('should not be resting anymore after 25 + 5 minutes', function() {
+				clock.tick(5 * MINUTE_IN_MS);
+				expect(subject.resting()).to.be.false;
+			});
+		});
+
 	});
 
 	describe('interrupt', function() {
@@ -119,6 +150,9 @@ describe('Pomodoro', function() {
 		it('should be stopped', function(){
 			expect(subject.running()).to.be.false;
 		});
+		it('should not be resting', function(){
+			expect(subject.resting()).to.be.false;
+		});
 		it('should emit an interrupted event', function() {
 			expect(interruptListener).to.have.been.calledOnce;
 		});
@@ -126,15 +160,81 @@ describe('Pomodoro', function() {
 			expect(interruptListener).to.have.been.calledWith(subject);
 		});
 		it('should not emit ticked event every minute', function(){
-			clock.tick(5 * 1000);
+			clock.tick(5 * MINUTE_IN_MS);
 			expect(tickListener).to.not.have.been.called;
 		});
 		it('should not emit stopped event after 25 minutes', function(){
-			clock.tick(25 * 1000);
+			clock.tick(25 * MINUTE_IN_MS);
 			expect(stopListener).to.not.have.been.called;
 		});
 		it('should throw Exception if already stopped', function() {
 			expect(subject.interrupt.bind(subject)).to.throw('Cannot stop pomodoro already stopped');
 		});
+	});
+
+	describe('configuration', function() {
+		var clock;
+		var listener;
+
+		beforeEach(function() {
+			clock = sinon.useFakeTimers();
+			listener = sinon.spy();
+		});
+		afterEach(function() {
+			clock.restore();
+			listener.reset();
+		});
+
+		describe('custom activity duration in minutes', function() {
+			beforeEach(function() {
+				subject = new Pomodoro(30);
+				subject.start();
+				subject.on('stopped', listener);
+			});
+
+			it('should not emit stopped event before 30 minutes', function() {
+				clock.tick(30 * MINUTE_IN_MS - 1);
+				expect(listener).to.not.have.been.called;
+			});
+			it('should emit stopped event after 30 minutes', function() {
+				clock.tick(30 * MINUTE_IN_MS);
+				expect(listener).to.have.been.called;
+			});
+		});
+
+		describe('custom rest duration in minutes', function() {
+			beforeEach(function() {
+				subject = new Pomodoro(25, 10);
+				subject.start();
+				subject.on('restEnded', listener);
+			});
+
+			it('should not emit restEnded event before 35 minutes', function() {
+				clock.tick(35 * MINUTE_IN_MS - 1);
+				expect(listener).to.not.have.been.called;
+			});
+			it('should emit stopped event after 35 minutes', function() {
+				clock.tick(35 * MINUTE_IN_MS);
+				expect(listener).to.have.been.called;
+			});
+		});
+
+		describe('custom tick duration in seconds', function() {
+			beforeEach(function() {
+				subject = new Pomodoro(25, 5, 2);
+				subject.start();
+				subject.on('ticked', listener);
+			});
+
+			it('should not emit ticked event before 2 seconds', function() {
+				clock.tick(2 * 1000 - 1);
+				expect(listener).to.not.have.been.called;
+			});
+			it('should emit ticked event after 2 seconds', function() {
+				clock.tick(2 * 1000);
+				expect(listener).to.have.been.called;
+			});
+		});
+
 	});
 });
